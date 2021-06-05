159f612ebc36291b5b8b461ac334ac942a639a64
fujiyasu0522 <522yasuaki@gmail.com>
2021-06-05 21:47:36 +0900

    商品情報編集機能
    ログインしていないユーザーがURLにeditを打ちこんだ時
    https://gyazo.com/c25eef74fe7c9e6d989240a89116c939
    
    出品者以外がURLにeditを打ち込んだ時
    https://gyazo.com/a1b65071b58130fbdf067adb5d0cc307
    
    エラーハンドリング
    https://gyazo.com/3143e40c30248ca4f9dfe9057d921d22
    
    未変更でも画像が抜けない
    https://gyazo.com/7c69fd1d4c46655961f7ab6f9c7713ec
    
    商品情報の編集
    https://gyazo.com/fa986c2182bfaa2dd6916a5f0d9a09a7

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create]
-  before_action :set_item, only:[:show]
-
+  before_action :set_item, only:[:show, :edit, :update]
+  
   def index
     @items = Item.all.order(created_at: "DESC")
   end
@@ -20,6 +20,24 @@ class ItemsController < ApplicationController
       else
         render :new
       end
+  end
+
+  def edit
+    if user_signed_in?
+      if current_user.id != @item.user_id
+        redirect_to action: :index
+      end
+    else
+      redirect_to user_session_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
